e6ffa43612d2879bfb944b90ac95103860bb3694
brightendev@hotmail.com <brightendev@hotmail.com>
2019-03-18 01:56:28 +0700

    Authentication/Authorization เพิ่มเติม

---
+++ KJobRecruitmentWebApp/System/User/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace KJobRecruitmentWebApp.System.User
+{
+    public class Account
+    {
+
+        public Account() {
+            
+        }
+
+
+
+    }
+}
