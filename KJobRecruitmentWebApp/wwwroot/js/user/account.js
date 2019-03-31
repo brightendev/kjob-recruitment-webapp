307072713eb6058190d0208493df14a0a01ea3a2
MoonCake <Krissada.man@gmail.com>
2019-03-31 22:25:18 +0700

    เพิ่ม dialog ต่างๆ

--- KJobRecruitmentWebApp/wwwroot/js/user/account.js
+++ KJobRecruitmentWebApp/wwwroot/js/user/account.js
@@ -1,13 +1,22 @@
 
 
 //// ===== notification settings ==========
+
+
 $(function () {
+    //============ edit notification section ===========
+
+
+
+
+
+     /*---------------------------------------- edit notification-all----------------------------------------*/
     $('#notification-all').on('click', function () {
         Swal.fire({
-            title: 'ตั้งค่าการแจ้งเตือน',
+            title: 'ตั้งค่าการแจ้งเตือนทั้งหมด',
             html: '<div class="switch"><label>OFF<input type="checkbox" id="status" checked><span class="lever"></span>ON</label></div>',
             focusConfirm: false,
-            confirmButtonText: 'บัยทึก',
+            confirmButtonText: 'บันทึก',
             showCancelButton: true,
             cancelButtonText: 'ยกเลิก',
             showLoaderOnConfirm: true,
@@ -15,44 +24,379 @@ $(function () {
             // Ajax request
             preConfirm: () => {
                 var status = $('#status').is(':checked');
+
+                /* check status*/
+                if (status) {
+                    document.getElementById("notification-status").innerHTML = "เปิดใช้งานอยู่";
+
+                }
+                else {
+                    document.getElementById("notification-status").innerHTML = "ปิดใช้งานอยู่";
+                }
                 console.log(status);
-                return fetch(`/ajax/set_notification_all`, 
-                        {
-                            method: "GET", 
-                            mode: "cors", 
-                            cache: "no-cache", 
-                            credentials: "same-origin", 
-                            headers: { "Content-Type": "application/json" }, 
-                        }
-                )
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(response.statusText);
+                return fetch(`/ajax/set_notification_all`,
+                    {
+                        method: "GET",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
+                        headers: { "Content-Type": "application/json" },
                     }
-                    return response.text();
-                })
-                .catch(error => {
-                    Swal.showValidationMessage(
-                        `Request failed: ${error}`
-                    );
-                });
+                )
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.text();
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
             }
         })
-        // result
-        .then((result) => {
-            if (result.value == 'success') {
-                console.log(result.value);
-                Swal.fire({
-                    type: 'success',
-                    title: `แก้ไขแล้ว`
-                });
-            }
-            if (result.value == 'error') {
-                Swal.fire({
-                    type: 'error',
-                    title: 'เกิดข้อผิดพลาด'
-                });
-            }
-        });
+            // result
+            .then((result) => {
+                if (result.value == 'success') {
+                    console.log(result.value);
+                    Swal.fire({
+                        type: 'success',
+                        title: `แก้ไขแล้ว`
+                    });
+                }
+                if (result.value == 'error') {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'เกิดข้อผิดพลาด'
+                    });
+                }
+            });
     });
+    /*----------------------------------------end of edit notification-all----------------------------------------*/
+
+
+    /*----------------------------------------edit email notification----------------------------------------*/
+    $('#notification-email').on('click', function () {
+        Swal.fire({
+            title: 'ตั้งค่าการแจ้งเตือนอีเมล์',
+            html: '<div class="switch"><label>OFF<input type="checkbox" id="status" checked><span class="lever"></span>ON</label></div>',
+            focusConfirm: false,
+            confirmButtonText: 'บันทึก',
+            showCancelButton: true,
+            cancelButtonText: 'ยกเลิก',
+            showLoaderOnConfirm: true,
+
+            // Ajax request
+            preConfirm: () => {
+                var status = $('#status').is(':checked');
+
+                /* check status*/
+                if (status) {
+                    document.getElementById("notification-statusEmail").innerHTML = "เปิดใช้งานอยู่";
+                }
+                else {
+                    document.getElementById("notification-statusEmail").innerHTML = "ปิดใช้งานอยู่";
+                }
+                console.log(status);
+                return fetch(`/ajax/set_notification_email`,
+                    {
+                        method: "GET",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
+                        headers: { "Content-Type": "application/json" },
+                    }
+                )
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.text();
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
+            }
+        })
+            // result
+            .then((result) => {
+                if (result.value == 'success') {
+                    console.log(result.value);
+                    Swal.fire({
+                        type: 'success',
+                        title: `แก้ไขแล้ว`
+                    });
+                }
+                if (result.value == 'error') {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'เกิดข้อผิดพลาด'
+                    });
+                }
+            });
+    });
+    /*----------------------------------------end of edit email notification----------------------------------------*/
+
+     /*----------------------------------------edit  notification-news----------------------------------------*/
+    $('#notification-news').on('click', function () {
+        Swal.fire({
+            title: 'ตั้งค่าการแจ้งเตือนข่าวสาร',
+            html: '<div class="switch"><label>OFF<input type="checkbox" id="status" checked><span class="lever"></span>ON</label></div>',
+            focusConfirm: false,
+            confirmButtonText: 'บันทึก',
+            showCancelButton: true,
+            cancelButtonText: 'ยกเลิก',
+            showLoaderOnConfirm: true,
+
+            // Ajax request
+            preConfirm: () => {
+                var status = $('#status').is(':checked');
+
+                /* check status*/
+                if (status) {
+                    document.getElementById("notification-newsStatus").innerHTML = "เปิดใช้งานอยู่";
+                }
+                else {
+                    document.getElementById("notification-newsStatus").innerHTML = "ปิดใช้งานอยู่";
+                }
+                console.log(status);
+                return fetch(`/ajax/set_notification_news`,
+                    {
+                        method: "GET",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
+                        headers: { "Content-Type": "application/json" },
+                    }
+                )
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.text();
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
+            }
+        })
+            // result
+            .then((result) => {
+                if (result.value == 'success') {
+                    console.log(result.value);
+                    Swal.fire({
+                        type: 'success',
+                        title: `แก้ไขแล้ว`
+                    });
+                }
+                if (result.value == 'error') {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'เกิดข้อผิดพลาด'
+                    });
+                }
+            });
+    });
+    /*----------------------------------------end of edit  notification-news ----------------------------------------*/
+
+
+    /*----------------------------------------edit notification-interested-job----------------------------------------*/
+    $('#notification-interested-job').on('click', function () {
+        Swal.fire({
+            title: 'ตั้งค่าการแจ้งเตือนข่าวสาร',
+            html: '<div class="switch"><label>OFF<input type="checkbox" id="status" checked><span class="lever"></span>ON</label></div>',
+            focusConfirm: false,
+            confirmButtonText: 'บันทึก',
+            showCancelButton: true,
+            cancelButtonText: 'ยกเลิก',
+            showLoaderOnConfirm: true,
+
+            // Ajax request
+            preConfirm: () => {
+                var status = $('#status').is(':checked');
+
+                /* check status*/
+                if (status) {
+                    document.getElementById("notification-interested-jobStatus").innerHTML = "เปิดใช้งานอยู่";
+                }
+                else {
+                    document.getElementById("notification-interested-jobStatus").innerHTML = "ปิดใช้งานอยู่";
+                }
+                console.log(status);
+                return fetch(`/ajax/set_notification_jobstatus`,
+                    {
+                        method: "GET",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
+                        headers: { "Content-Type": "application/json" },
+                    }
+                )
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.text();
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
+            }
+        })
+            // result
+            .then((result) => {
+                if (result.value == 'success') {
+                    console.log(result.value);
+                    Swal.fire({
+                        type: 'success',
+                        title: `แก้ไขแล้ว`
+                    });
+                }
+                if (result.value == 'error') {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'เกิดข้อผิดพลาด'
+                    });
+                }
+            });
+    });
+    /*----------------------------------------end of edit notification-interested-job----------------------------------------*/
+
+
+
+
+
+    // ============= edit profile section ============================================================================================
+
+    /*edit account*/
+    $('#account-email').on('click', function () {
+        Swal.fire({
+            title: 'ตั้งค่าการแจ้งเตือน',
+            html: '<div ><input type="text" class="form-control" id="dataEmail" </div>',
+            focusConfirm: false,
+            confirmButtonText: 'บันทึก',
+            showCancelButton: true,
+            cancelButtonText: 'ยกเลิก',
+            showLoaderOnConfirm: true,
+            // Ajax request
+            preConfirm: () => {
+                var data = $('#dataEmail').val();
+                console.log(data);
+                return fetch(`/ajax/set_email`,
+                    {
+                        method: "GET",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
+                        headers: { "Content-Type": "application/json" },
+                    }
+                )
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.text();
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
+            }
+        })
+            // result
+            .then((result) => {
+                if (result.value == 'success') {
+                    console.log(result.value);
+                    Swal.fire({
+                        type: 'success',
+                        title: `แก้ไขแล้ว`
+                    });
+                }
+                if (result.value == 'error') {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'เกิดข้อผิดพลาด'
+                    });
+                }
+            });
+        
+    });
+    /*end edit email*/
+
+
+    /*edit password*/
+    $('#account-pass').on('click', function () {
+        Swal.fire({
+            title: 'ตั้งค่าการแจ้งเตือน',
+            html: '<div ><input type="text" class="form-control" id="dataPass" </div>',
+            focusConfirm: false,
+            confirmButtonText: 'บันทึก',
+            showCancelButton: true,
+            cancelButtonText: 'ยกเลิก',
+            showLoaderOnConfirm: true,
+            // Ajax request
+            preConfirm: () => {
+                var data = $('#dataPass').val();
+                console.log(data);
+                return fetch(`/ajax/set_password`,
+                    {
+                        method: "GET",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
+                        headers: { "Content-Type": "application/json" },
+                    }
+                )
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.text();
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
+            }
+        })
+            // result
+            .then((result) => {
+                if (result.value == 'success') {
+                    console.log(result.value);
+                    Swal.fire({
+                        type: 'success',
+                        title: `แก้ไขแล้ว`
+                    });
+                }
+                if (result.value == 'error') {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'เกิดข้อผิดพลาด'
+                    });
+                }
+            });
+
+    });
+    /*end of edit passoword*/
+
+
+
+
+
+
+
+
+
+
+
+    //end of all function
 });
